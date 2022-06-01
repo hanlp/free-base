4cf7344a83700a3f0538eb92e1a8f582f4f459dc
hanlp <852410674@qq.com>
2022-06-01 16:38:45 +0800

    save

--- src/main/java/com/java/base/HelloWorld.java
+++ src/main/java/com/java/base/HelloWorld.java
@@ -3,5 +3,6 @@ package com.java.base;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world...");
+        System.out.println("end");
     }
 }
